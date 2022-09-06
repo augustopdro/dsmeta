bbb1f3190242ba072b5606f8edfb3eaa9950b484
augustopdro <rm94990@fiap.com.br>
2022-09-06 17:11:52 -0300

    First component.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,12 @@
+import NotificationButton from "./components/NotificationButton";
+
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <NotificationButton />
+    </>
+  );
 }
 
 export default App;
