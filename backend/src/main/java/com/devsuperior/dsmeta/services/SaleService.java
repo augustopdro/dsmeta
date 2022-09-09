39f27e79c1e34780e2224805be418a04194a7f38
augustopdro <rm94990@fiap.com.br>
2022-09-08 21:50:14 -0300

    API test

---
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -0,0 +1,19 @@
+package com.devsuperior.dsmeta.services;
+
+import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.repositories.SaleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SaleService {
+
+    @Autowired
+    private SaleRepository repository;
+
+    public List<Sale> findSales(){
+        return repository.findAll();
+    }
+}
