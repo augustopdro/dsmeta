547e310bdb9a7114d1e3e3b03a4ee7a320fbd153
augustopdro <rm94990@fiap.com.br>
2022-09-09 10:16:02 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from "./components/Header";
-import NotificationButton from "./components/NotificationButton";
 import SalesCard from "./components/SalesCard";
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
